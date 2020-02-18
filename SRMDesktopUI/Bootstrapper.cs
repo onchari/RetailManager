d350710bcf61dec5389f6d4ef6ca8e285c35bb16
onchari <onchariwycliffe@gmail.com>
2020-02-18 22:30:54 +0300

    Added the SSDT & WPF Desktop UI

---
+++ SRMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Caliburn.Micro;
+using SRMDesktopUI.ViewModels;
+using System.Windows;
+
+namespace SRMDesktopUI
+{
+    class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+
+        }
+    }
+}
